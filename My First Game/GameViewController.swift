ed9591832280e6d607af4b39fa0c65af58d432fa
Sami <sami@samis-mbp.home>
2017-06-18 18:26:31 +0100

    First Commit

--- My First Game/GameViewController.swift
+++ My First Game/GameViewController.swift
@@ -14,6 +14,7 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Let's push this to github")
         
         // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
         // including entities and graphs.
